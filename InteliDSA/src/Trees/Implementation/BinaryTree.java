1cb9efd87a0369d608405b63ac917c3f1867658c
Amit <amitkumaramitkumar15351@gmail.com>
2025-09-14 01:38:43 +0530

    Implementing binaryTree papulate method to insert the node.values here  is how it works: first it ask to insert the root node simple and for the first time it created the root node and while creating it addeds the values .................... and it papulate another papulate private function which is actually inserting values left or right this is like helper function of recursion you got the point ............. now here is ask first do you wan tinsert left if you yes yes then  i will ask you to enter the values and later it will create a nedd node pay attention  NODE.LEFT=NEW NODE(VAL) by this is created a new node and also make the chain from the node here passed like i nthis case it wass root node so values is assigned to node and chain is created again this will call this papulate function recursively and keep in mind this time passed node is left node suppose last time you have insert 15 as root and you have inserted 6 as left............. again the call happen with 6 wali node and same ask you left or right if you again said this time yes left so again will do same and add the valeus to root 15 left 6 left 8 okey now again recursive call ................. but with 8 node will ask do you want insert left you said now so this will go next and ask .............. with 8 node do you want insert right you said no so i goes back...... 8 is out now will call for 6 and 6 left is called ......... right is remaning so call do you want insert right you said yes 9 so insert by creating new node as node.right=new Node(val) chain maked with 6 node and will again for 9 ........do you want left you said false .........call again right you said false out of 9 and .... out of 6 as well because both call end now ask did you want right of 15 which is root node you said false recursive call over and ended the insertion function

--- InteliDSA/src/Trees/Implementation/BinaryTree.java
+++ InteliDSA/src/Trees/Implementation/BinaryTree.java
@@ -16,12 +16,6 @@ public class BinaryTree {
     }
     private Node root;
     // insert elements
-    /* its concepts only
-    public void insert(int value){
-        insert(left);
-        insert(right);
-    }
-    */
     public void papulate(Scanner scanner){
         System.out.println("Enter the root node: ");
         int val=scanner.nextInt();
@@ -29,6 +23,28 @@ public class BinaryTree {
         //first time take root and for the others take left or right
         papulate(scanner,root);
     }
+    /*Implementing binaryTree papulate method to insert the node.values here  is
+    how it works: first it ask to insert the root node simple and for the first time
+    it created the root node and while creating it addeds the values ..................
+    .. and it papulate another papulate private function which is actually inserting
+    values left or right this is like helper function of recursion you got the point ......
+    ....... now here is ask first do you wan tinsert left if you yes yes then  i will ask
+    you to enter the values and later it will create a nedd node pay attention
+    NODE.LEFT=NEW NODE(VAL) by this is created a new node and also make the chain
+    from the node here passed like i nthis case it wass root node so values is assigned to
+    node and chain is created again this will call this papulate function recursively and keep
+    in mind this time passed node is left node suppose last time you have insert 15 as root
+    and you have inserted 6 as left............. again the call happen with 6 wali node and
+    same ask you left or right if you again said this time yes left so again will do same and
+    add the valeus to root 15 left 6 left 8 okey now again recursive call ................. but
+     with 8 node will ask do you want insert left you said now so this will go next and ask .
+     ............. with 8 node do you want insert right you said no so i goes back...... 8 is
+     out now will call for 6 and 6 left is called ......... right is remaning so call do you want
+      insert right you said yes 9 so insert by creating new node as node.right=new Node(val)
+      chain maked with 6 node and will again for 9 ........do you want left you said false ....
+      .....call again right you said false out of 9 and .... out of 6 as well because both call
+      end now ask did you want right of 15 which is root node you said false recursive call over
+       and ended the insertion function*/
     private void papulate(Scanner scanner,Node node){//left right  chaining elplanation
         System.out.println("Do you want to insert left of "+ node.value);
         boolean left=scanner.nextBoolean();//true means on left side false means i don't want insert left
