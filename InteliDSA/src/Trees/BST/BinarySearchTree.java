78938e57ca9772041643f8d57eff656e89010e55
Amit <amitkumaramitkumar15351@gmail.com>
2025-09-16 14:39:38 +0530

    implementing BST 16 sep 2025

--- InteliDSA/src/Trees/BST/BinarySearchTree.java
+++ InteliDSA/src/Trees/BST/BinarySearchTree.java
@@ -1,5 +1,20 @@
 package Trees.BST;
 
 public class BinarySearchTree {
-    
+    private class Node{
+        int value;
+        int height;
+        Node left;
+        Node right;
+        public Node(int val){
+            this.value=val;
+        }
+        public int getValue(){
+            return value;
+        }
+    }
+    private Node root;
+    public BinarySearchTree(){
+
+    }
 }
