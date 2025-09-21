662291cf7751cbaf7176f5e28a39f548239ccd2e
Amit <amitkumaramitkumar15351@gmail.com>
2025-09-21 23:04:16 +0530

    applied query method so specify the index waana work and implemented update methodd to update the values 21 sep 2025

--- InteliDSA/src/Trees/SegmentTree/Main.java
+++ InteliDSA/src/Trees/SegmentTree/Main.java
@@ -7,6 +7,8 @@ public class Main {
         SegmentTree st=new SegmentTree(arr);
         st.display();
         System.out.println(st.query(2,6));
+        st.update(5,0);
+        System.out.println(st.query(2,6));//-8 replaced with 0 so now values should be +8
 
     }
 }
