990c5793c413db33138325c8a89158f9d0e24a54
Amit <amitkumaramitkumar15351@gmail.com>
2025-09-21 16:52:06 +0530

    like suppose we are given an array and ask to find sum of all num in array or in a range so it will take O(N) right sum+=arr[i] which is O(N) 21 sep 2025

---
+++ InteliDSA/src/Trees/SegmentTree/Tree.java
@@ -0,0 +1,12 @@
+package Trees.SegmentTree;
+/*
+*  why segment tree:            like suppose we are given an array and ask to find sum of all
+*                                num in array or in a range so it will take O(N) right sum+=arr[i]
+*                               which is O(N) 21 sep 2025
+*                               sum+=arr[i] if o(N) not so good
+* use cases:                    when we are asked to do something in a range sike sum of array or
+*                               max of min or product of all no many like this we can do by segment
+*                               tree in o(LogN) isntead of O(N)
+* */
+public class Tree {
+}
