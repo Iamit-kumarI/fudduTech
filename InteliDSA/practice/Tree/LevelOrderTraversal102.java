87d14bb932f09b7333ac0f30e8c091dab3b35d63
Amit <amitkumaramitkumar15351@gmail.com>
2025-09-24 06:13:47 +0530

    ltc  102 solved tree problem for the first time 24 sep 2025

--- InteliDSA/practice/Tree/LevelOrderTraversal102.java
+++ InteliDSA/practice/Tree/LevelOrderTraversal102.java
@@ -1,7 +1,45 @@
 package Tree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class LevelOrderTraversal102 {
-    public static void main(String[] args) {
-        System.out.println("hi");
+    public class TreeNode {
+     int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result=new ArrayList<>();
+        if(root==null)return result;
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+        while(queue.isEmpty()){
+            int levelSize=queue.size();
+            List<Integer> clist=new ArrayList<>(levelSize);
+
+            for(int i=0;i<levelSize;i++){
+                TreeNode cNode=queue.poll();
+                clist.add(cNode.val);
+                if(cNode.left!=null){
+                    queue.offer(cNode.left);
+                }
+                if(cNode.right!=null){
+                    queue.offer(cNode.left);
+                }
+            }
+            result.add(clist);
+        }
+
+        return result;
     }
 }
