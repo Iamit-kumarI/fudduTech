aff1059f6c0834f45fc9cb3fb2dc80d7624306d4
Amit <amitkumaramitkumar15351@gmail.com>
2025-10-02 23:59:44 +0530

    h

---
+++ InteliDSA/practice/Tree/DFS/PathSum.java
@@ -0,0 +1,12 @@
+package Tree.DFS;
+
+import Tree.TreeNode;
+/*
+public class PathSum extends TreeNode {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root==null)return false;
+        if(root.val==sum&&root.left==null&&root.right==null)return true;
+        return hasPathSum(root.left,sum-root.val)||hasPathSum(root.right,sum-root.val);
+    }
+}
+ */
