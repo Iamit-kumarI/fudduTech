1db5502dae1de46698dc52d916c64caf32415260
Amit <amitkumaramitkumar15351@gmail.com>
2025-10-10 04:57:38 +0530

    implemented range query

--- InteliDSA/practice/Algos/RangeQuery.java
+++ InteliDSA/practice/Algos/RangeQuery.java
@@ -1,4 +1,42 @@
 package Algos;
 
+import java.util.Arrays;
+
 public class RangeQuery {
+    public static void main(String[] args) {
+        int [] arr={1,3,5,2,7,6,3,1,4,8};
+        int len=arr.length;
+        int sqrt=(int)Math.sqrt(len);
+        int blocksIndex=-1;
+        int []blocks=new int[sqrt+1];
+        for(int i=0;i<len;i++){
+            if(i%sqrt==0){//new block starts here
+                blocksIndex++;
+            }
+            blocks[blocksIndex]+=arr[i];
+        }
+        System.out.println(query(blocks,arr,2,7,3));
+    }
+    public static int query(int []blocks,int []arr,int left,int right,int sqrt){
+        int ans=0;
+        //left case
+        while(left%sqrt!=0&&left<right&&left!=0){
+            ans+=arr[left++];
+        }
+        //middle part
+        while(left+sqrt<=right){
+            ans+=blocks[left/sqrt];
+            left+=sqrt;
+        }
+        //right part
+        while(left<=right){
+            ans+=arr[left++];
+        }
+        return ans;
+    }
+    public static void update(int []blocks,int []arr,int index,int val,int sqrt){
+        int blockindex=index/sqrt;
+        blocks[blockindex]+=(val-arr[index]);
+        arr[index]=val;
+    }
 }
