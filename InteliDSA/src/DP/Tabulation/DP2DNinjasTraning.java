9c24a1df1f4d449583649dc33efc60b675ef58d8
Amit <amitkumaramitkumar15351@gmail.com>
2025-10-26 19:55:55 +0530

    2D Dp Ninjas Traning DP 7 came to sc O(N)+O(n*4) -> O(n*4) -> O(4) almost const space

--- InteliDSA/src/DP/Tabulation/DP2DNinjasTraning.java
+++ InteliDSA/src/DP/Tabulation/DP2DNinjasTraning.java
@@ -4,6 +4,26 @@ import java.util.Arrays;
 
 public class DP2DNinjasTraning {
     public static int ninjaTraining(int n, int [][]points) {
+        int [][]dp=new int[n][4];
+        dp[0][0]=Math.max(points[0][1],points[0][2]);
+        dp[0][1]=Math.max(points[0][0],points[0][2]);
+        dp[0][2]=Math.max(points[0][1],points[0][0]);
+        dp[0][3]=Math.max(points[0][1],Math.max(points[0][2],points[0][0]));
+
+        for(int day=1;day<n;day++){
+            for(int last=0;last<4;last++){
+                dp[day][last]=0;
+                int totalMax=0;
+                for(int task=0;task<3;task++){
+                    if(task!=last){
+                        totalMax=Math.max(totalMax,points[day][task]+dp[day-1][task]);
+                    }
+                }
+                dp[day][last]=totalMax;
+            }
+        }
+        return dp[n-1][3];
+        /*
         int [][]arr=new int[n][4];
 //        Arrays.fill(arr,-1);
         for(int i=0;i<n;i++){
@@ -31,7 +51,7 @@ public class DP2DNinjasTraning {
             }
         }
         return dp[day][last]=totalMax;
+         */
     }
 }
 
-}
