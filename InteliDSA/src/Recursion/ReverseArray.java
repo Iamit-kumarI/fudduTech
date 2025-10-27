7ab4648954053873c30467be77b609d63f49bbbf
Amit <amitkumaramitkumar15351@gmail.com>
2025-10-27 10:59:19 +0530

    sum of array using recursion competetive coding

--- InteliDSA/src/Recursion/ReverseArray.java
+++ InteliDSA/src/Recursion/ReverseArray.java
@@ -1,6 +1,14 @@
 package Recursion;
 
-//Striver Re4
 public class ReverseArray {
     //reverse using recursion
+    public static void main(String[] args) {
+        int []num={1,2,3,4,5,6,7,8,9,10};
+        System.out.println(helper(num,0,0));
+    }
+    public static int helper(int []arr,int index,int sum){
+        if(index==arr.length-1)return sum;
+//        sum+=arr[index];
+        return helper(arr,index+1,sum+arr[index]);
+    }
 }
