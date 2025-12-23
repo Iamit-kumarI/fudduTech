9bf7b1cabe7cdb2227042c1d2fb15e70a6267f37
Amit <amitkumaramitkumar15351@gmail.com>
2025-12-23 12:24:35 +0530

    shany problem 908, Day 58, 23 Dec 2025

---
+++ InteliDSA/practice/Leetcode/Math/Ltc908Sunny.java
@@ -0,0 +1,12 @@
+package Leetcode.Math;
+
+public class Ltc908Sunny {
+    public int smallestRangeI(int[] nums, int k) {
+        int min=Integer.MAX_VALUE,max=Integer.MIN_VALUE;
+        for(int val:nums){
+            if(val<min)min=val;
+            if(val>max)max=val;
+        }
+        return (min+k)-(max-k)<0?0:(max-k)-(min+k);
+    }
+}
