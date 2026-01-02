21cb1c03715423a035ab667017e0a29e303f45ba
Amit <amitkumaramitkumar15351@gmail.com>
2026-01-02 23:31:07 +0530

    POTD updated, Day 68 2 Jan 2026

--- InteliDSA/src/DP/Subset/partitionEqualSubsetSum.java
+++ InteliDSA/src/DP/Subset/partitionEqualSubsetSum.java
@@ -1,27 +1,31 @@
-//package DP.Subset;
-//
-//import java.util.Arrays;
-//public class partitionEqualSubsetSum {
-//    public int minimumDifference(int[] nums) {
-//        int sum=0,n=nums.length;
-//        for(int val:nums)sum+=val;
-//        boolean[][]dp=new boolean[n][sum+1];
-//        for(int i=0;i<n;i++)dp[i][0]=true;
-//        if(nums[0]<=sum)dp[0][nums[0]]=true;
-//        for(int index=1;index<n;index++){
-//            for(int taget=1;taget<=sum;taget++){
-//                boolean nottake=dp[index-1][taget];
-//                boolean take=false;
-//                if(nums[index]<=taget){
-//                    take=dp[index-1][taget-nums[index]];
-//                }
-//                dp[index][taget]=take||nottake;
-//            }
-//        }
-//        int min=Integer.MAX_VALUE;
-//        for(int s1=0;s1<=sum/2;s1++){
-//            if(dp[n-1][s1])min=Math.min(min,Math.abs(sum-2*s1);
-//        }
-//        return min;
-//    }
-//}
+package DP.Subset;
+
+import java.util.Arrays;
+public class partitionEqualSubsetSum {
+    public int minimumDifference(int[] nums) {
+        int n=nums.length,totalsum=0;
+        for(int val:nums)totalsum+=val;
+        boolean[][]arr=new boolean[n][totalsum+1];
+        for(int i=0;i<=totalsum;i++){
+            subSetSum(n-1,i,nums,arr);
+        }
+        int min=Integer.MAX_VALUE;
+        for(int s1=0;s1<=totalsum;s1++){
+            if(arr[n-1][s1]){
+                int s2=totalsum-s1;
+                min=Math.min(min,Math.abs(s1-s2));
+            }
+        }
+        return min;
+    }
+
+    private boolean subSetSum(int index,int target,int[] nums, boolean[][] dp) {
+        if(target==0)return dp[index][target]=true;
+        if(index==0)return dp[index][target]=(nums[0]==target);
+        if(dp[index][target])return dp[index][target];
+        boolean nottake=subSetSum(index-1,target,nums,dp);
+        boolean take=false;
+        if(nums[index]<=target)take=subSetSum(index-1,target-nums[index],nums,dp);
+        return dp[index][target]=take||nottake;
+    }
+}
