ae45f388ecb27e3d0bfcc7f9f33f6b8c5d4c60bf
Amit <amitkumaramitkumar15351@gmail.com>
2026-01-05 17:48:28 +0530

    Terminal Operations

---
+++ InteliDSA/src/Collection/Streams/TerminalOperations.java
@@ -0,0 +1,44 @@
+package Collection.Streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class TerminalOperations {
+    public static void main(String[] args) {
+        //taking output/result from stream
+        List<Integer> list= Arrays.asList(2,3,4,4);
+
+        //1. Collect
+        Set<Integer> collectedSet = list.stream().skip(1).collect(Collectors.toSet());
+        collectedSet.add(5);
+        System.out.println(collectedSet);
+
+        //2. forEach
+        list.stream().forEach(x-> System.out.println(x+" "));
+
+        //3. reduce : combines results to produce single result
+//        Optional<Integer> reducedStrea = list.stream().reduce((x, y) -> x + y);
+        Optional<Integer> reducedStrea = list.stream().reduce(Integer::sum);//convreted lambda into stream working is still same
+        System.out.println(reducedStrea.get().equals(13));//total sum is 13 so 13==13 right
+
+        //4. count we have seen
+
+        //5. anyMatch(), allMatch(), nonMatch()
+        boolean countIsAnyElementInListIsEven = list.stream().anyMatch(x -> x % 2 == 0);
+        System.out.println(countIsAnyElementInListIsEven);
+
+        boolean countIsAllElementsInListIsEven = list.stream().allMatch(x -> x % 2 == 0);
+        System.out.println(countIsAllElementsInListIsEven);
+
+        boolean noNegativeValue = list.stream().noneMatch(x -> x <0);
+        System.out.println(noNegativeValue);//checks list dosen't have negative values
+
+        //6. findFirst, findAny
+        System.out.println(list.stream().findFirst().get());//returns first element onlu
+        System.out.println(list.stream().findAny().get());//return any element
+
+    }
+}
