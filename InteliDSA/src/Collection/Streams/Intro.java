5b7afeaefd15f443f8cd9619cc270094f1090c07
Amit <amitkumaramitkumar15351@gmail.com>
2026-01-05 15:32:47 +0530

    Stream Theory

--- InteliDSA/src/Collection/Streams/Intro.java
+++ InteliDSA/src/Collection/Streams/Intro.java
@@ -2,6 +2,15 @@ package Collection.Streams;
 
 public class Intro {
     public static void main(String[] args) {
+        // Stream introduces in Java 8
+        // By the help of stream we can process the collection of data in a functional and
+        //declearative way
+        // 1. simplifies data processing(if else wagera)
+        // 2. work on functional programming
+        // 3. Improve readability and maintainability
+        // 4. Enable Easy Parallelsim with useing going in multithreading
 
+        //How to implement it?
+        
     }
 }
