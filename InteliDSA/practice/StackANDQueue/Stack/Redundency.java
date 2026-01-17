6a768e9b955dd0b9d86c11d4065f3be7aa0f7ebe
Amit <amitkumaramitkumar15351@gmail.com>
2026-01-17 16:01:52 +0530

    Expression contains redundant bracket or not , Day 84, 17 Jan 2026

---
+++ InteliDSA/practice/StackANDQueue/Stack/Redundency.java
@@ -0,0 +1,36 @@
+package StackANDQueue.Stack;
+
+import java.util.Stack;
+
+public class Redundency {
+    public static void main(String[] args) {
+        String str="(a+(b+(c+d)))";
+        System.out.println(checkRedundancy(str));
+    }
+    public static boolean checkRedundancy(String s) {
+        Stack<StringBuilder> st = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                st.push(new StringBuilder("("));
+            }
+            else if(s.charAt(i)==')'){
+                st.peek().append(")");
+                StringBuilder sb = st.peek();st.pop();
+                if(sb.length()>3){
+                    if(st.empty()){
+                        st.push(new StringBuilder("a"));
+                    }
+                    else st.peek().append("a");
+                }
+                else{
+                    return true;
+                }
+            }
+            else{
+                if(st.empty())st.push(new StringBuilder());
+                st.peek().append(s.charAt(i));
+            }
+        }
+        return false;
+    }
+}
