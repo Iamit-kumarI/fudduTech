919c8c8f42d880df314913c3222a5b37c943a926
Amit <amitkumaramitkumar15351@gmail.com>
2026-01-21 13:03:58 +0530

    revised flood fill

---
+++ InteliDSA/practice/Graph/DFS/FloodFill.java
@@ -0,0 +1,36 @@
+package Graph.DFS;
+
+import java.lang.classfile.attribute.LineNumberInfo;
+import java.util.Arrays;
+
+public class FloodFill {
+    public static void main(String[] args) {
+        int[][]arr={
+                {1,1,1},
+                {1,1,0},
+                {1,0,1}
+        };
+        arr=floodFill(arr,1,1,2);
+        for(int[]row:arr){
+            System.out.println(Arrays.toString(row));
+        }
+    }
+    public static int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int initialColor=image[sr][sc];
+        int[][]arr=Arrays.copyOf(image,image.length);
+        int[]allowedToGoRow={0,-1,1,0};
+        int[]allowedToGoCol={-1,0,0,1};
+        helperDfs(arr,sr,sc,color,initialColor,allowedToGoRow,allowedToGoCol);
+        return arr;
+    }
+    public static void helperDfs(int[][]arr,int row,int col,int newColor,int initialColor,int[]allowedToGoRow,int[]allowedToGoCol){
+        arr[row][col]=newColor;
+        for(int i=0;i<4;i++){
+            int newRow=row+allowedToGoRow[i];
+            int newCol=col+allowedToGoCol[i];
+            if(newRow>=0&&newRow<arr.length&&newCol>=0&&newCol<arr[0].length&&arr[newRow][newCol]!=newColor&&arr[newRow][newCol]==initialColor){
+                helperDfs(arr,newRow,newCol,newColor,initialColor,allowedToGoRow,allowedToGoCol);
+            }
+        }
+    }
+}
