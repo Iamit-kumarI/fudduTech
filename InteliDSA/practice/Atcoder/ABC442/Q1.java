1663399fae1d124d16658294494874cb6a504e4c
Amit <amitkumaramitkumar15351@gmail.com>
2026-01-24 20:31:29 +0530

    abc 442 updated

--- InteliDSA/practice/Atcoder/ABC442/Q1.java
+++ InteliDSA/practice/Atcoder/ABC442/Q1.java
@@ -1,7 +1,13 @@
 package Atcoder.ABC442;
 
+import java.util.Scanner;
+
 public class Q1 {
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        int ans=0;
+        for(char c:s.toCharArray())if(c=='i'||c=='j')ans++;
+        System.out.println(ans);
     }
 }
