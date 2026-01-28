aa9dbf1b9ba022f29aea883010fea3da8238ca60
Amit <amitkumaramitkumar15351@gmail.com>
2026-01-28 06:44:47 +0530

    Three Sum done

--- InteliDSA/practice/NeetCodeSheet/TwoPointer/TwoSumII.java
+++ InteliDSA/practice/NeetCodeSheet/TwoPointer/TwoSumII.java
@@ -1,4 +1,20 @@
 package NeetCodeSheet.TwoPointer;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class TwoSumII {
+    public static void main(String[] args) {
+        int[]arr={2,7,11,15};
+        System.out.println(Arrays.toString(twoSum(arr,9)));
+    }
+    public static int[] twoSum(int[] numbers, int target) {
+        int start=0,end=numbers.length-1;
+        while(start<end){
+            if(numbers[start]+numbers[end]==target)return new int[]{start+1,end+1};
+            if(numbers[start]+numbers[end]>target)end--;
+            if(numbers[start]+numbers[end]<target)start++;
+        }
+        return new int[]{-1,-1};
+    }
 }
