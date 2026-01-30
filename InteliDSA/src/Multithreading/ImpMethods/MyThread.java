ae3e51ac65af208a7c61e20c5f0651ba8fcc48ff
Amit <amitkumaramitkumar15351@gmail.com>
2026-01-30 23:45:07 +0530

    Thread t1.setDeamon()

--- InteliDSA/src/Multithreading/ImpMethods/MyThread.java
+++ InteliDSA/src/Multithreading/ImpMethods/MyThread.java
@@ -19,5 +19,7 @@ public class MyThread extends Thread{
         once we set current thread deamon it kills it self how once it created t1  and set deamon
         done now it will start so as it will start it will go to the @Override run() and done some
         work in parallel same time it print main done now it comes to know nothing  left in main to
-        do and i am set to deamon so it will destroy it self
+        do and i am set to deamon so it will destroy it self Means main thread: so it check all the work
+        is done now it won't wait for others to finish or do background task it will terminate if no thred
+        left in main(current thread)
  */
