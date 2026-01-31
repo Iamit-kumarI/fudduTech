dd69cdb3a5c0fab7e81b8531cc934177c93bc928
Amit <amitkumaramitkumar15351@gmail.com>
2026-01-31 22:34:51 +0530

    BW 175 updated updated

--- InteliDSA/practice/Leetcode/Biweekly/BW175/Q1.java
+++ InteliDSA/practice/Leetcode/Biweekly/BW175/Q1.java
@@ -2,6 +2,40 @@ package Leetcode.Biweekly.BW175;
 
 public class Q1 {
     public static void main(String[] args) {
-
+        String str=")ebc#da@f(";
+        System.out.println(reverseByType(str));
+    }
+    public static String reverseByType(String s) {
+        char[]charArray=s.toCharArray();
+        int low=0,end=charArray.length-1;
+        while(low<end){
+            if(!Character.isAlphabetic(charArray[low])){
+                low++;
+                continue;
+            }
+            if(!Character.isAlphabetic(charArray[end])){
+                end--;
+                continue;
+            }
+            char temp=charArray[low];
+            charArray[low++]=charArray[end];
+            charArray[end--]=temp;
+        }
+        low=0;
+        end=charArray.length-1;
+        while(low<end){
+            if(Character.isAlphabetic(charArray[low])){
+                low++;
+                continue;
+            }
+            if(Character.isAlphabetic(charArray[end])){
+                end--;
+                continue;
+            }
+            char temp=charArray[low];
+            charArray[low++]=charArray[end];
+            charArray[end--]=temp;
+        }
+        return new String(charArray);
     }
 }
--- InteliDSA/practice/Leetcode/Biweekly/BW175/Q2.java
+++ InteliDSA/practice/Leetcode/Biweekly/BW175/Q2.java
@@ -1,4 +1,29 @@
 package Leetcode.Biweekly.BW175;
 
 public class Q2 {
+    public static void main(String[] args) {
+        int[]arr={3,7,5};
+        System.out.println(minimumK(arr));
+    }
+    public static int minimumK(int[] nums) {
+        int n=nums.length,maximum=0;
+        int start=1;
+        for(int val:nums)if(val>maximum)maximum=val;
+        int end=Math.max(n,maximum);
+        int ans=end;
+        while (start<=end){
+            int mid=start+(end-start)/2;
+            long count=0;
+            for(int val:nums){
+                count+=(val+mid-1)/mid;
+            }
+            if(count<=(long)mid*mid){
+                ans=mid;
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
 }
