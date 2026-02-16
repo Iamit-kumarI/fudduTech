77814386f6f2b4aa9623cdc698c1af84461f15d0
Amit <amitkumaramitkumar15351@gmail.com>
2026-02-17 00:11:33 +0530

    reverse bit

--- InteliDSA/practice/String/Temp.java
+++ InteliDSA/practice/String/Temp.java
@@ -1,22 +1,13 @@
 package String;
 
+import java.util.*;
+
 public class Temp {
     public static void main(String[] args) {
-        String str="Ram isdkf asdljf boy";
-        String[]sarr=str.split("\\s+");
-        StringBuilder sb=new StringBuilder();
-        for(String cur:sarr){
-            sb.append(revWord(cur));
-        }
-        System.out.println(sb.toString());
-    }
-    public static String revWord(String word){
-        int s=0,end=word.length()-1;
-        StringBuilder sb=new StringBuilder(word);
-        while(s<end){
-            sb.setCharAt(s,word.charAt(end));
-            sb.setCharAt(end--,word.charAt(s++));
-        }
-        return sb.append(" ").toString();
+        //LRU practice
+        List<Integer> list=new ArrayList<>();
+        for(int i=1;i<10;i++)list.add(i);
+        //creting vertual ngnix server and upload custom data(custom html)
+        //Exp : vertual serval+ngnix with user data script
     }
 }
