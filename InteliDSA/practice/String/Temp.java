bd0ba528b91d2642ea56ea34386dd71307f669e9
Amit <amitkumaramitkumar15351@gmail.com>
2026-02-18 14:52:44 +0530

    alternate bits solved

--- InteliDSA/practice/String/Temp.java
+++ InteliDSA/practice/String/Temp.java
@@ -1,13 +1,9 @@
 package String;
-
 import java.util.*;
+
 
 public class Temp {
     public static void main(String[] args) {
-        //LRU practice
-        List<Integer> list=new ArrayList<>();
-        for(int i=1;i<10;i++)list.add(i);
-        //creting vertual ngnix server and upload custom data(custom html)
-        //Exp : vertual serval+ngnix with user data script
+
     }
 }
--- InteliDSA/src/Collection/Set/SET/Seet.java
+++ InteliDSA/src/Collection/Set/SET/Seet.java
@@ -1,7 +1,17 @@
 package Collection.Set.SET;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Seet {
     public static void main(String[] args) {
-
+        Map<Integer,String>db=new HashMap<>();
+        db.put(101,"Amit");
+        db.put(102,"Aditi");
+        db.put(103,"Rahul");
+        System.out.println(db);
+        for(Map.Entry<Integer,String>enteries:db.entrySet()){
+            System.out.print(enteries.getValue()+" ");
+        }
     }
 }
