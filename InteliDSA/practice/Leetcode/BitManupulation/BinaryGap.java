3abc8748f624cea5965d08441af99a518ac1a97e
Amit <amitkumaramitkumar15351@gmail.com>
2026-02-23 00:16:09 +0530

    binary gap

--- InteliDSA/practice/Leetcode/BitManupulation/BinaryGap.java
+++ InteliDSA/practice/Leetcode/BitManupulation/BinaryGap.java
@@ -1,10 +1,21 @@
 package Leetcode.BitManupulation;
 
+import java.util.Stack;
+
 public class BinaryGap {
     public static void main(String[] args) {
-
+        System.out.println(binaryGap(8));
     }
     public static int binaryGap(int n) {
-
+        int max=0,cur=0,index=-1;
+        while(n>0){
+            if((n&1)==1){
+                if(index!=-1)max=Math.max(max,cur-index);
+                index=cur;
+            }
+            cur++;
+            n>>=1;
+        }
+        return max;
     }
 }
