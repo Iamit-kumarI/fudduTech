2d021df12ecbb52c198ee236943c4b1ab5e9df92
Amit <amitkumaramitkumar15351@gmail.com>
2026-02-23 15:16:30 +0530

    binary code

--- InteliDSA/practice/Leetcode/BitManupulation/BinaryGap.java
+++ InteliDSA/practice/Leetcode/BitManupulation/BinaryGap.java
@@ -7,15 +7,20 @@ public class BinaryGap {
         System.out.println(binaryGap(8));
     }
     public static int binaryGap(int n) {
-        int max=0,cur=0,index=-1;
-        while(n>0){
-            if((n&1)==1){
-                if(index!=-1)max=Math.max(max,cur-index);
-                index=cur;
+        String s=Integer.toBinaryString(n);
+        int count=0,max=0;//will finx this soon
+        boolean onefound=false;
+        for(char c:s.toCharArray()){
+            if(c=='0'&&onefound){
+                count++;
+                continue;
+            }else if(c=='1'){
+                if(onefound)count=1;
+                onefound=true;
+
             }
-            cur++;
-            n>>=1;
+            max=Math.max(max,count);
         }
-        return max;
+        return max==0?0:max+1;
     }
 }
