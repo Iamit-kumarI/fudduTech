0b6bbd5946b531263d3cc82847197c16aa910985
Amit <amitkumaramitkumar15351@gmail.com>
2026-03-10 00:30:28 +0530

    updated disjoint set by size plus rank

---
+++ InteliDSA/src/Graph/DisJointSet/MakingALargeIsland.java
@@ -0,0 +1,14 @@
+package Graph.DisJointSet;
+
+public class MakingALargeIsland {
+    public static void main(String[] args) {
+        int[][]arr={
+                {1,0},
+                {0,1}
+        };
+        System.out.println(largestIsland(arr));
+    }
+    public static int largestIsland(int[][] grid) {
+
+    }
+}
