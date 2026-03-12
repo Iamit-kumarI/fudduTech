f9ab51dd7229306a2918ae277091259dcdb1930f
Amit <amitkumaramitkumar15351@gmail.com>
2026-03-12 22:33:21 +0530

    united we stand and longest reapeating character both are left yet

---
+++ InteliDSA/practice/Codeforces/R800/Buttons.java
@@ -0,0 +1,13 @@
+package Codeforces.R800;
+
+import java.util.Scanner;
+
+public class Buttons {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            
+        }
+    }
+}
