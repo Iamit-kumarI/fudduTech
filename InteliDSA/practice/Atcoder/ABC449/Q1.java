65be984c7014280be143d8e1f05966853d367dbe
Amit <amitkumaramitkumar15351@gmail.com>
2026-03-14 19:54:03 +0530

    abc 449 updated

---
+++ InteliDSA/practice/Atcoder/ABC449/Q1.java
@@ -0,0 +1,12 @@
+package Atcoder.ABC449;
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double pi=Math.PI;
+        int d=sc.nextInt();
+        System.out.println((pi*Math.pow(d,2)/4));
+    }
+}
