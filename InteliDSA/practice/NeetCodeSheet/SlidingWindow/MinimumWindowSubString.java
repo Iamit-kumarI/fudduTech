59c000f8129660744da8812734e1c021c2af4dcc
Amit <amitkumaramitkumar15351@gmail.com>
2026-04-01 00:05:56 +0530

    subarray product less than k apply two pointer and sliding window

---
+++ InteliDSA/practice/NeetCodeSheet/SlidingWindow/MinimumWindowSubString.java
@@ -0,0 +1,10 @@
+package NeetCodeSheet.SlidingWindow;
+
+public class MinimumWindowSubString {
+    public static void main(String[] args) {
+        System.out.println(minWindow("",""));
+    }
+    public static String minWindow(String s, String t) {
+        return "1";
+    }
+}
