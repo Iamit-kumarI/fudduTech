fafa8ec145175332dab51e71a85d21083eca991b
Amit <amitkumaramitkumar15351@gmail.com>
2026-04-01 23:33:55 +0530

    product sliding window

---
+++ InteliDSA/practice/Temp/Temp.java
@@ -0,0 +1,12 @@
+package Temp;
+
+import java.util.Arrays;
+
+public class Temp {
+    public static void main(String[] args) {
+        System.out.println(attack(7,3));
+    }
+    public static int attack(int a,int b){
+        return 1;
+    }
+}
